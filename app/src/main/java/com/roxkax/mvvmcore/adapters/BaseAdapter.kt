786f1cc66161d63aca7a2b8c82c85982a5042b22
Diogo Gonçalves <rxkax@hotmail.com>
2020-01-16 14:00:07 +0000

    added baseAdapter

---
+++ app/src/main/java/com/roxkax/mvvmcore/adapters/BaseAdapter.kt
@@ -0,0 +1,39 @@
+package com.roxkax.mvvmcore.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Base class to reduce some of the boilerplate for creating a [RecyclerView.Adapter]
+ */
+class BaseAdapter<T, B : ViewDataBinding>(
+    private val items: List<T>,
+    private val itemsResourceId: Int,
+    private val onBind: (B, T) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        object : RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                itemsResourceId,
+                parent,
+                false
+            )
+        ) {}
+
+
+    /**
+     * Returns the number of elements of the source collection of the [RecyclerView.Adapter].
+     * @return [Int] of with number of items
+     */
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        DataBindingUtil.bind<B>(holder.itemView)?.let {
+            onBind(it, items[position])
+        }
+    }
+}
