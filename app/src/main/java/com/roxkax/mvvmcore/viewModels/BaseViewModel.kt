7a7cc35400872cd6d61598f34cc928ee551c87da
Diogo Gonçalves <rxkax@hotmail.com>
2020-02-05 17:41:05 +0000

    added activity tracker to base viewmodel

--- app/src/main/java/com/roxkax/mvvmcore/viewModels/BaseViewModel.kt
+++ app/src/main/java/com/roxkax/mvvmcore/viewModels/BaseViewModel.kt
@@ -1,8 +1,18 @@
 package com.roxkax.mvvmcore.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.roxkax.mvvmcore.activities.BaseActivity
+import com.roxkax.mvvmcore.services.ActivityTrackerService
+import javax.inject.Inject
 
 /**
  * A [ViewModel] with some common logic used in Mvvm applications
  */
-abstract class BaseViewModel : ViewModel()
+abstract class BaseViewModel : ViewModel() {
+
+    @Inject
+    lateinit var activityTrackerService: ActivityTrackerService
+
+
+    val activity: BaseActivity? get() = activityTrackerService.currentActivity
+}
